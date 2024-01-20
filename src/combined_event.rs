cae58247a0182513cebd46bb6f0804956a2159b6
Shaun Remekie <shaun.remekie@coralogix.com>
2024-01-20 03:06:10 +0100

    simplify comebined event serialization

--- src/combined_event.rs
+++ src/combined_event.rs
@@ -1,17 +1,19 @@
-use aws_lambda_events::event::cloudwatch_logs::AwsLogs;
+use aws_lambda_events::event::cloudwatch_logs::LogsEvent;
 use aws_lambda_events::event::s3::S3Event;
 use aws_lambda_events::event::sns::SnsEvent;
 use aws_lambda_events::event::sqs::SqsEvent;
 use aws_lambda_events::event::kinesis::KinesisEvent;
+use aws_lambda_events::event::kafka::KafkaEvent;
 use serde::de::{self, Deserialize, Deserializer};
 use serde_json::Value;
 
 pub enum CombinedEvent {
     S3(S3Event),
     Sns(SnsEvent),
-    CloudWatchLogs(AwsLogs),
+    CloudWatchLogs(LogsEvent),
     Sqs(SqsEvent),
     Kinesis(KinesisEvent),
+    Kafka(KafkaEvent),
 }
 
 impl<'de> Deserialize<'de> for CombinedEvent {
@@ -21,37 +23,33 @@ impl<'de> Deserialize<'de> for CombinedEvent {
     {
         let raw_value: Value = Deserialize::deserialize(deserializer)?;
 
-        // Attempt to match against known event structures event_source = "aws:sqs"
-        tracing::debug!("raw_value: {:?}", raw_value);
-        if let Some(records) = raw_value.get("Records") {
-            if records[0].get("s3").is_some() {
-                Ok(CombinedEvent::S3(
-                    S3Event::deserialize(raw_value).map_err(de::Error::custom)?,
-                ))
-            } else if records[0].get("Sns").is_some() {
-                Ok(CombinedEvent::Sns(
-                    SnsEvent::deserialize(raw_value).map_err(de::Error::custom)?,
-                ))
-            } else if records[0].get("eventSource").map_or(false, |v| v == "aws:sqs") {
-                Ok(CombinedEvent::Sqs(
-                    SqsEvent::deserialize(raw_value).map_err(de::Error::custom)?,
-                ))
-            } else if records[0].get("eventSource").map_or(false, |v| v == "aws:kinesis") {
-                Ok(CombinedEvent::Kinesis(
-                KinesisEvent::deserialize(raw_value).map_err(de::Error::custom)?,
-                ))
-            } else {
-                Err(de::Error::custom("Unknown Records event type"))
-            }
-        } else if let Some(records) = raw_value.get("awslogs") {
-            tracing::debug!("records: {:?}", records);
-            Ok(CombinedEvent::CloudWatchLogs(
-                AwsLogs::deserialize(records).map_err(de::Error::custom)?,
-            ))
-            //Ok(CombinedEvent::CloudWatchLogs(
-            //    AwsLogs::deserialize(raw_value).map_err(de::Error::custom)?,
-        } else {
-            Err(de::Error::custom("Unknown event type"))
+        if let Ok(event) = S3Event::deserialize(&raw_value) {
+            tracing::debug!("s3 event detected");
+            return Ok(CombinedEvent::S3(event));
         }
+
+        if let Ok(event) = SnsEvent::deserialize(&raw_value) {
+            tracing::debug!("sns event detected");
+            return Ok(CombinedEvent::Sns(event));
+        }
+
+        if let Ok(event) = LogsEvent::deserialize(&raw_value) {
+            tracing::debug!("cloudwatch event detected");
+            return Ok(CombinedEvent::CloudWatchLogs(event));
+        }
+
+        if let Ok(event) = KafkaEvent::deserialize(&raw_value) {
+            tracing::debug!("kafka event detected");
+            return Ok(CombinedEvent::Kafka(event));
+        }
+
+        if let Ok(event) = SqsEvent::deserialize(&raw_value) {
+            tracing::debug!("sqs event detected");
+            return Ok(CombinedEvent::Sqs(event));
+        }
+
+        Err(de::Error::custom(format!(
+            "unsupported event type: {raw_value}"
+        )))
     }
 }
