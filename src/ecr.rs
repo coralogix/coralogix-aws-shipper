a170913bc2793352b090725e220e3726adb61738
juan-coralogix <juan@coralogix.com>
2024-01-29 11:05:22 -0300

    Summery Added

--- src/ecr.rs
+++ src/ecr.rs
@@ -1,33 +1,24 @@
-use crate::process::Metadata;
-use crate::*;
 use serde::{Serialize, Deserialize};
-use aws_lambda_events::cloudwatch_logs::AwsLogs;
-use aws_lambda_events::ecr_scan::{EcrScanEvent, self};
-use aws_lambda_events::encodings::Base64Data;
-use aws_sdk_ecr::types::ImageScanFinding;
-use aws_sdk_ecr::types::ImageIdentifier;
-use aws_sdk_s3::Client;
+use aws_lambda_events::ecr_scan::EcrScanEvent;
 use aws_sdk_ecr::Client as EcrClient;
-use cx_sdk_rest_logs::DynLogExporter;
-use fancy_regex::Regex;
-use flate2::read::MultiGzDecoder;
-use itertools::Itertools;
 use lambda_runtime::Error;
-use std::ffi::OsStr;
-use std::io::Read;
-use std::ops::Range;
-use std::path::Path;
 use std::string::String;
-use std::time::Instant;
-use tracing::{debug, info, warn};
+use tracing::{debug, info};
+use aws_sdk_ecr::types::ImageIdentifier;
 
 use crate::config::{Config, IntegrationType};
 use crate::coralogix;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+struct CurrentCountRoot {
+    metadata: Ecrmetadata,
+    ecr_scan_summary: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Root {
     metadata: Ecrmetadata,
-    finding: Findings,
+    findings: Findings,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -89,6 +80,8 @@ pub async fn process_ecr_scan_event(
     config: &Config,
     ecr_client: &EcrClient,
 ) -> Result<Vec<String>, Error> {
+    let current_severity_count = event.detail.finding_severity_counts.clone();
+    info!("current_severity_count: {:?}", current_severity_count);
     let current_ecr_metadata = Ecrmetadata {
         repository: event.detail.repository_name.unwrap_or("".to_string()),
         image_id: ImageId {
@@ -106,57 +99,65 @@ pub async fn process_ecr_scan_event(
         .image_id(image_identifier)
         .send();
     let response = request.await?;
+    let image_scan_findings = response.image_scan_findings.unwrap();
 
     let mut logs: Vec<String> = Vec::new(); // Initialize logs outside of the loop
 
-    if let Some(findings) = response.image_scan_findings.unwrap().findings {
+    if let Some(findings) = image_scan_findings.findings {
         for finding in findings {
-            info!("Finding: {:?}", finding);
             if let Some(attributes) = finding.attributes {
                 let mut current_attributes: Vec<Attribute> = Vec::new();
                 let mut current_name: String = "NO_PACKAGE".to_string();
                 let mut current_version: String = "NO_VERSION".to_string();
                 for attribute in attributes {
-                    info!("Attribute: {:?}", attribute);
                     let attribute_value = attribute.value; 
                     if current_name == "NO_PACKAGE" && attribute.key == "package_name" {
                         current_name = attribute_value.clone().unwrap_or("NO_PACKAGE".to_string());
                     }
-                    if current_version == "NO_VERSION" {
-                        if attribute.key == "package_version" {
-                            current_version = attribute_value.clone().unwrap_or("NO_VERSION".to_string());
-                        }
+                    if current_version == "NO_VERSION" && attribute.key == "package_version"{
+                        current_version = attribute_value.clone().unwrap_or("NO_VERSION".to_string());
                     }
                     
                     current_attributes.push(Attribute {
                         key: attribute.key,
                         value: attribute_value.unwrap_or("".to_string()),
                     });
-                    let package = Package {
-                        package: format!("{}:{}", current_name.clone(), current_version),
-                        name: current_name.clone(),
-                        version: current_version.clone(),
-                    };
-                let current_details = Details {
-                    name: finding.name.clone().unwrap_or("".to_string()),
-                    uri: finding.uri.clone().unwrap_or("".to_string()),
-                    severity: finding.severity.clone().map_or("".to_string(), |v| v.as_str().to_string()),
-                    attributes: current_attributes.clone(),
-                };
-                let findings = Findings {
-                    package: package,
-                    details: current_details,
-                };
-                let root = Root {
-                    metadata: current_ecr_metadata.clone(),
-                    finding: findings,
-                };
-                info!("Root: {:?}", root);
-                logs.push(serde_json::to_string(&root).unwrap());
-                }  
-            }   
+                }
+            let package = Package {
+                package: format!("{}:{}", current_name.clone(), current_version),
+                name: current_name.clone(),
+                version: current_version.clone(),
+            };
+                   
+            let current_details = Details {
+                name: finding.name.clone().unwrap_or("".to_string()),
+                uri: finding.uri.clone().unwrap_or("".to_string()),
+                severity: finding.severity.clone().map_or("".to_string(), |v| v.as_str().to_string()),
+                attributes: current_attributes.clone(),
+            };
+            let findings = Findings {
+                package: package,
+                details: current_details,
+            };
+            let root = Root {
+                metadata: current_ecr_metadata.clone(),
+                findings: findings,
+            };
+            logs.push(serde_json::to_string(&root).unwrap());
+            }  
+        }   
+        if let Some(finding_severity_counts) = image_scan_findings.finding_severity_counts {
+            let ecr_scan_summary: Vec<String> = finding_severity_counts.iter()
+                .map(|(severity, count)| format!("{:?}: {}", severity, count))
+                .collect();
+            
+            let current_count_root = CurrentCountRoot {
+                metadata: current_ecr_metadata,
+                ecr_scan_summary: ecr_scan_summary,
+            };
+        logs.push(serde_json::to_string(&current_count_root).unwrap());
         }
     }
-
-    Ok(logs) // Return the logs as the result of the function
+    
+    Ok(logs)
 }
