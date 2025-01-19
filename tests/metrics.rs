af408d4da18f06286ba77364bea63a4cc302cdf2
Shaun Remekie <shaun.remekie@coralogix.com>
2025-01-19 01:52:36 +0100

    add unit test for metrics workflow

--- tests/metrics.rs
+++ tests/metrics.rs
@@ -1,79 +1,111 @@
 use wiremock;
-use async_trait::async_trait;
-// use wiremock::matchers::{method, path, body};
 use serde_json;
 use coralogix_aws_shipper::metrics::config::Config;
 use lambda_runtime::{Context, LambdaEvent};
 use coralogix_aws_shipper::events;
+use std::sync::Arc;
+
+
+use opentelemetry_proto::tonic::collector::metrics::v1::ExportMetricsServiceRequest; // Example path for trace data
+use opentelemetry_proto::tonic::metrics::v1::metric::Data::Summary;
+
+use opentelemetry_proto::tonic::common::v1::any_value;
+use prost::Message;
+
 
 async fn run_test_firehose_transform_flow() {
-
-    let event = serde_json::json!({
-      "invocationId": "invocation-id-123456",
-      "deliveryStreamArn": "arn:aws:firehose:region:account-id:deliverystream/stream-name",
-      "region": "us-east-1",
-      "records": [
-        {
-          "recordId": "record-id-1234",
-          "approximateArrivalTimestamp": 1701234567,
-          "data": "eyJtZXRyaWMiOiAiQ1BVVXNhZ2UiLCAidmFsdWUiOiAxMjMuNDU2LCAidGltZXN0YW1wIjogIjIwMjQtMDYtMTFUMTM6MDA6MDBaIn0="
-        },
-        {
-          "recordId": "record-id-5678",
-          "approximateArrivalTimestamp": 1701234568,
-          "data": "eyJtZXRyaWMiOiAiTGF0ZW5jeSIgLCAidmFsdWUiOiAzMjAuNzgsICJ0aW1lc3RhbXAiOiAiMjAyNC0wNi0xMVQxMzowMDowMFoifQ=="
-        }
-      ]
-    });
-
-    let bytes = vec![130, 94, 10, 255, 93, 10, 186, 1, 10, 23, 10, 14, 99, 108, 111, 117, 100, 46, 112, 114, 111, 118, 105, 100, 101, 114, 18, 5, 10, 3, 97, 119, 115, 10, 34, 10, 16, 99, 108, 111, 117, 100, 46, 97, 99, 99, 111, 117, 110, 116, 46, 105, 100, 18, 14, 10, 12, 55, 55, 49, 48, 51, 57, 54, 52, 57, 52, 52, 48, 10, 27, 10, 12, 99, 108, 111, 117, 100, 46, 114, 101, 103, 105, 111, 110, 18, 11, 10, 9, 101, 117, 45, 119, 101, 115, 116, 45, 49];
-
-
-    let result = String::from_utf8(bytes.clone());
-    match result {
-        Ok(string) => println!("String: {}", string),
-        Err(e) => println!("Invalid UTF-8: {}", e),
-    }
-
-    // let event = serde_json::json!({
-    //   "invocationId": "595b024c-949a-40c1-ae67-dbc825afc66c",
-    //   "deliveryStreamArn": "arn:aws:firehose:eu-west-1:771039649440:deliverystream/MetricStreams-QuickFull-KKn5QV-nJq6VxcV",
-    //   "sourceKinesisStreamArn": null,
-    //   "region": "eu-west-1",
-    //   "records": [
-    //     {
-    //       "recordId": "shardId-00000000000000000000000000000000000000000000000000000000000000000000000000000000000049657715769015733549787243891873794809635777967011397634000000000000",
-    //       "approximateArrivalTimestamp": "2024-12-17T13:24:07.033Z",
-    //       "data": "eyJtZXRyaWMiOiAiTGF0ZW5jeSIgLCAidmFsdWUiOiAzMjAuNzgsICJ0aW1lc3RhbXAiOiAiMjAyNC0wNi0xMVQxMzowMDowMFoifQ=="
-    //     }
-    //   ]
-    // });
-
+    let data = std::fs::read("./tests/fixtures/cwstream_metrics.json").unwrap();
     let config = Config::load_from_env().unwrap();
-    let evt: events::Combined = serde_json::from_value(event).unwrap();
+    let evt: events::Combined = serde_json::from_slice(&data).unwrap();
     let evt = LambdaEvent::new(evt, Context::default());
     coralogix_aws_shipper::metrics::handler(&config, evt).await.unwrap();
-
 }
 
 
 #[tokio::test]
 async fn test_firehose_transform_flow() {
-    let mock_server = wiremock::MockServer::start().await;
 
+    let message_count = Arc::new(std::sync::Mutex::new(0));
+    let counter = message_count.clone();
+    // let (tx, mut rx) = tokio::sync::mpsc::channel::<Vec<u8>>(2);
+
+    let server = wiremock::MockServer::start().await;
     wiremock::Mock::given(wiremock::matchers::method("POST"))
-        .and(wiremock::matchers::path("/"))
-        .and(wiremock::matchers::body_json(r#"{"a": "b"}"#)) 
+        .and(wiremock::matchers::path("/v1/metrics"))
+        .and(move |r: &wiremock::Request| -> bool {
+
+            // increment message count
+            let mut count = counter.lock().unwrap();
+
+            let buffer = r.body.clone();
+            let metrics = ExportMetricsServiceRequest::decode(&*buffer).unwrap();
+
+            for resource in metrics.resource_metrics {
+                for scope_metrics in resource.scope_metrics {
+                  for metric in scope_metrics.metrics.iter() {
+
+                    assert!(metric.unit == "");
+                    let data = metric.data.clone().ok_or("no metric data").unwrap();
+                    // let summary = data.summary.ok_or("no summary").unwrap();
+                    
+                    match data {
+                        Summary(summary) => {
+                            summary.data_points.iter().for_each(|dp| {
+                                let expected_sub_name = any_value::Value::StringValue("testsubsystem".to_string());
+                                let expected_app_name = any_value::Value::StringValue("testapp".to_string());
+                                
+                                // asert cx.application.name is in labels
+                                assert!(dp.attributes.iter().any(|label| {
+                                    label.key == "cx.application.name"
+                                }) == true);
+                                
+                                // asert cx.subsystem.name is in labels
+                                assert!(dp.attributes.iter().any(|label| {
+                                    label.key == "cx.subsystem.name"
+                                }) == true);
+
+                                dp.attributes.iter().for_each(|label| {
+                                    if label.key == "cx.application.name" {
+                                        let v = label.value.clone()
+                                            .ok_or("no label value for metric")
+                                            .unwrap()
+                                            .value
+                                            .unwrap();
+                                        assert!(v == expected_app_name);                                   
+                                    }
+
+                                    if label.key == "cx.subsystem.name" {
+                                        
+                                        let v = label.value.clone()
+                                            .ok_or("no label value for metric")
+                                            .unwrap()
+                                            .value
+                                            .unwrap();
+
+                                        assert!(v == expected_sub_name);                                    
+                                    }
+                                })
+                            })
+                        },
+                        _ => continue
+                    }
+                  }
+                }
+            }
+            *count += 1;
+            true
+        })
         .respond_with(wiremock::ResponseTemplate::new(200))
-        .mount(&mock_server)
+        .mount(&server)
         .await;
 
 
     temp_env::async_with_vars(
         [
             ("CORALOGIX_API_KEY", Some("1234456789X")),
-            ("APP_NAME", Some(mock_server.uri().as_str())),
-            ("CORALOGIX_DOMAIN", Some("localhost:8080")),
+            ("APP_NAME", Some("testapp")),
+            ("SUB_NAME", Some("testsubsystem")),
+            ("CORALOGIX_ENDPOINT", Some(server.uri().as_str())),
             ("INTEGRATION_TYPE", Some("S3")),
             ("AWS_REGION", Some("eu-central-1")),
             ("TELEMETRY_MODE", Some("metrics")),
@@ -81,4 +113,7 @@ async fn test_firehose_transform_flow() {
         run_test_firehose_transform_flow(),
     )
     .await;
+
+    assert_eq!(message_count.lock().unwrap().clone(), 2);
 }
+
