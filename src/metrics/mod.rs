131def2fdcd0cc8df582f4776ee60481780655b9
Shaun Remekie <shaun.remekie@coralogix.com>
2025-01-28 14:50:17 +0100

    update kinesis_firehose function name

--- src/metrics/mod.rs
+++ src/metrics/mod.rs
@@ -13,7 +13,7 @@ pub async fn handler(
     event: LambdaEvent<events::Combined>,
 ) -> Result<KinesisFirehoseResponse, Error> {
     match event.payload {
-        events::Combined::Firehose(firehose_event) => process::kinesis_firehose(config, firehose_event).await,
+        events::Combined::Firehose(firehose_event) => process::transform_firehose_event(config, firehose_event).await,
         _ => {
             error!("incompatible event type for metrics telemetry mode");
             Err("incompatible event type for metrics telemetry mode".to_string().into())
