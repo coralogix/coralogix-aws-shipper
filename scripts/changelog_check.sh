1fb8069a00337e4b75c475cb0ead7b4e86221dfb
royfur <121816837+royfur@users.noreply.github.com>
2026-06-07 19:17:29 +0300

    preserve changelog diff errors

--- scripts/changelog_check.sh
+++ scripts/changelog_check.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 # Simply check if diff in changelog exists.
-if git diff --exit-code --quiet origin/master... ./CHANGELOG.md; then
+git diff --exit-code --quiet origin/master... ./CHANGELOG.md
+diff_status=$?
+
+if [ "$diff_status" -eq 0 ]; then
   echo "Following files have been changed:"
-  echo "$(git diff --name-only "origin/master..." ./)"
+  git diff --name-only "origin/master..." ./
   echo ""
   echo "Please add a changelog entry in CHANGELOG.md or add 'skip changelog' label to your PR if this change does not require an entry."
   exit 1
+fi
+
+if [ "$diff_status" -ne 1 ]; then
+  exit "$diff_status"
 fi
